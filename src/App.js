98ead4c81a016a08518398375495a20633e1b445
Karan Shetty <shettykaran2110@gmail.com>
2020-03-22 15:18:00 +0530

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Error from './pages/Error';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div>
-      <Home />
-      <Error />
-      <Rooms />
-      <SingleRoom />
-    </div>
+    <Router>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/rooms/">
+          <Rooms />
+        </Route>
+        <Route exact path="/rooms/:slug">
+          <SingleRoom />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
