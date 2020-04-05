6f92898cb2bfe7ab06135cbc0cb364f2247e6bca
Karan Shetty <shettykaran2110@gmail.com>
2020-04-05 22:18:03 +0530

    Created Contact Page

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 import Navbar from './components/Navbar';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { RoomProvider } from './context';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms/" component={Rooms} />
-        <Route exact path="/rooms/:slug" component={SingleRoom} />
-        <Route component={Error} />
-      </Switch>
-    </>
+    <RoomProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/rooms/" component={Rooms} />
+          <Route exact path="/rooms/:slug" component={SingleRoom} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={Error} />
+        </Switch>
+      </BrowserRouter>
+    </RoomProvider>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Title from '../components/Title';
+
+const About = () => {
+  return <div></div>;
+};
+
+export default About;
