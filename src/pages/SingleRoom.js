32b3c84ce68ba3495650611df948023f4e933685
Karan Shetty <shettykaran2110@gmail.com>
2020-10-13 02:19:28 +0530

    Used react hooks

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
 import defaultBcg from '../images/room-1.jpeg';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
@@ -6,90 +6,79 @@ import { RoomContext } from '../context';
 
 import StyledHero from '../components/StyledHero';
 
-export default class SingleRoom extends Component {
-  constructor(props) {
-    super(props);
-    console.log(this.props);
-    this.state = {
-      slug: this.props.match.params.slug,
-      defaultBcg: defaultBcg,
-    };
+const SingleRoom = (props) => {
+  const [slug] = useState(props.match.params.slug);
+
+  const { getRoom } = useContext(RoomContext);
+  const room = getRoom(slug);
+
+  if (!room) {
+    return (
+      <div className="error">
+        <h3> no such room could be found...</h3>
+        <Link to="/rooms" className="btn-primary">
+          back to rooms
+        </Link>
+      </div>
+    );
   }
-  static contextType = RoomContext;
+  const {
+    name,
+    description,
+    capacity,
+    size,
+    price,
+    extras,
+    breakfast,
+    pets,
+    images,
+  } = room;
+  const [...defaultImages] = images;
+  // console.log(defaultImages);
 
-  // componentDidMount() {
-  //   console.log(this.props);
-  // }
-  render() {
-    const { getRoom } = this.context;
-    const room = getRoom(this.state.slug);
-
-    if (!room) {
-      return (
-        <div className="error">
-          <h3> no such room could be found...</h3>
+  return (
+    <>
+      <StyledHero img={images[0] || defaultBcg}>
+        <Banner title={`${name} room`}>
           <Link to="/rooms" className="btn-primary">
             back to rooms
           </Link>
+        </Banner>
+      </StyledHero>
+      <section className="single-room">
+        <div className="single-room-images">
+          {defaultImages.map((item, index) => (
+            <img key={index} src={item} alt={name} />
+          ))}
         </div>
-      );
-    }
-    const {
-      name,
-      description,
-      capacity,
-      size,
-      price,
-      extras,
-      breakfast,
-      pets,
-      images,
-    } = room;
-    const [...defaultImages] = images;
-    console.log(defaultImages);
+        <div className="single-room-info">
+          <article className="desc">
+            <h3>details</h3>
+            <p>{description}</p>
+          </article>
+          <article className="info">
+            <h3>info</h3>
+            <h6>price : &#x20B9;{price}</h6>
+            <h6>size : {size} SQFT</h6>
+            <h6>
+              max capacity :
+              {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+            </h6>
+            <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+            <h6>{breakfast && 'free breakfast included'}</h6>
+          </article>
+        </div>
+      </section>
+      <section className="room-extras">
+        <h6>extras </h6>
+        <ul className="extras">
+          {extras.map((item, index) => (
+            <li key={index}>- {item}</li>
+          ))}
+        </ul>
+      </section>
+    </>
+  );
+};
 
-    return (
-      <>
-        <StyledHero img={images[0] || this.state.defaultBcg}>
-          <Banner title={`${name} room`}>
-            <Link to="/rooms" className="btn-primary">
-              back to rooms
-            </Link>
-          </Banner>
-        </StyledHero>
-        <section className="single-room">
-          <div className="single-room-images">
-            {defaultImages.map((item, index) => (
-              <img key={index} src={item} alt={name} />
-            ))}
-          </div>
-          <div className="single-room-info">
-            <article className="desc">
-              <h3>details</h3>
-              <p>{description}</p>
-            </article>
-            <article className="info">
-              <h3>info</h3>
-              <h6>price : &#x20B9;{price}</h6>
-              <h6>size : {size} SQFT</h6>
-              <h6>
-                max capacity :
-                {capacity > 1 ? `${capacity} people` : `${capacity} person`}
-              </h6>
-              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
-              <h6>{breakfast && 'free breakfast included'}</h6>
-            </article>
-          </div>
-        </section>
-        <section className="room-extras">
-          <h6>extras </h6>
-          <ul className="extras">
-            {extras.map((item, index) => (
-              <li key={index}>- {item}</li>
-            ))}
-          </ul>
-        </section>
-      </>
-    );
-  }
-}
+export default SingleRoom;
