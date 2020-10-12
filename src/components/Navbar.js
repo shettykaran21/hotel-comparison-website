19ffab2bf980d26b5b2a6ad9e2b873be914fbd09
Karan Shetty <shettykaran2110@gmail.com>
2020-10-13 02:05:28 +0530

    Used react hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaAlignRight } from 'react-icons/fa';
 import logo from '../images/bed.svg';
 
-export default class Navbar extends Component {
-  state = {
-    isOpen: false,
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
   };
 
-  handleToggle = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
-
-  render() {
-    return (
-      <nav className="navbar">
-        <div className="nav-center">
-          <div className="nav-header">
-            <Link to="/">
-              <img className="logo" src={logo} alt="Beach Resort" />
-            </Link>
-            <button
-              type="button"
-              className="nav-btn"
-              onClick={this.handleToggle}
-            >
-              <FaAlignRight className="nav-icon" />
-            </button>
-          </div>
-          <ul
-            className={this.state.isOpen ? 'nav-links show-nav' : 'nav-links'}
-          >
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/rooms">Rooms</Link>
-            </li>
-            {/* <li>
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img className="logo" src={logo} alt="Beach Resort" />
+          </Link>
+          <button type="button" className="nav-btn" onClick={handleToggle}>
+            <FaAlignRight className="nav-icon" />
+          </button>
+        </div>
+        <ul className={isOpen ? 'nav-links show-nav' : 'nav-links'}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/rooms">Rooms</Link>
+          </li>
+          {/* <li>
               <Link to="/about">About</Link>
             </li> */}
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
