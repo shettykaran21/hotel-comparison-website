d669b19dbfdbe36d7c9b60b38dcdbe8bac0ed776
Karan Shetty <shettykaran2110@gmail.com>
2020-10-13 02:07:47 +0530

    Used react hooks

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import Title from './Title';
 import { RoomContext } from '../context';
 import Room from './Room';
 import Loading from './Loading';
 
-export default class FeaturedRooms extends Component {
-  static contextType = RoomContext;
+const FeaturedRooms = () => {
+  const { loading, featuredRooms: rooms } = useContext(RoomContext);
 
-  render() {
-    let { loading, featuredRooms: rooms } = this.context;
+  rooms = rooms.map((room) => {
+    return <Room key={room.id} room={room} />;
+  });
+  return (
+    <section className="featured-rooms">
+      <Title title="featured rooms" />
+      <div className="featured-rooms-center">
+        {loading ? <Loading /> : rooms}
+      </div>
+    </section>
+  );
+};
 
-    rooms = rooms.map((room) => {
-      return <Room key={room.id} room={room} />;
-    });
-    return (
-      <section className="featured-rooms">
-        <Title title="featured rooms" />
-        <div className="featured-rooms-center">
-          {loading ? <Loading /> : rooms}
-        </div>
-      </section>
-    );
-  }
-}
+export default FeaturedRooms;
